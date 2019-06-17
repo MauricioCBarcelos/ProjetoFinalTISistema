e950f4ad4657b161a1bc1cf3ba94b72dc12b4e27
Augusto Mendonça Flausino <titoomf@gmail.com>
2019-06-17 10:25:30 -0300

    vAtualizacao

--- src/main/java/model/vo/ClienteVO.java
+++ src/main/java/model/vo/ClienteVO.java
@@ -1,44 +1,51 @@
 package model.vo;
 
 public class ClienteVO {
-	
+
 	private int idcliente;
 	private String nome;
 	private String cpf;
 	private String telefone;
-	private String celular;
-	
+	private String email;
+
 	public int getIdcliente() {
 		return idcliente;
 	}
+
 	public void setIdcliente(int idcliente) {
 		this.idcliente = idcliente;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	public String getCelular() {
-		return celular;
+
+	public String getEmail() {
+		return email;
 	}
-	public void setCelular(String celular) {
-		this.celular = celular;
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
-	
-	
 
 }
--- src/main/java/model/vo/TecnicoVO.java
+++ src/main/java/model/vo/TecnicoVO.java
@@ -1,38 +1,33 @@
 package model.vo;
 
 public class TecnicoVO {
-	
+
 	private int idtecnico;
 	private String nome;
 	private String telefone;
-	private String email;
-	
+
 	public int getIdtecnico() {
 		return idtecnico;
 	}
+
 	public void setIdtecnico(int idtecnico) {
 		this.idtecnico = idtecnico;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
-	
 
 }
